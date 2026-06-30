592e5772a55a756199d803bd2c8e580795c67f86
Mewpet <claire_gaddhayward@hotmail.co.uk>
2026-06-30 11:24:12 +0100

    fixed the ul li layout in html, fixed the hover on the menu icon

---
+++ js/mobile-dropdown-menu.js
@@ -0,0 +1,9 @@
+let nav = document.getElementById("nav");
+let menuButton = document.getElementById("menu-icon");
+
+menuButton.addEventListener("click", ()=>{
+    //if(menu.style.display=="")
+    //nav.style.display = "none";
+    console.log(nav.style.display);
+    console.log(nav.style);
+})
