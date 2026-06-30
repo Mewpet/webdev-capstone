58967c8dda46b7ee320a3b60163bd77c019726f7
Mewpet <claire_gaddhayward@hotmail.co.uk>
2026-06-30 12:26:07 +0100

    added current link colour change feature

--- js/mobile-dropdown-menu.js
+++ js/mobile-dropdown-menu.js
@@ -1,14 +1,20 @@
 let nav = document.getElementById("nav");
 let menuButton = document.getElementById("menu-icon");
+let closeMenuButton = document.getElementById("close-menu-icon");
 
 menuButton.addEventListener("click", ()=>{
-    //if(menu.style.display=="")
-    //nav.style.display = "none";
-    if (nav.classList.contains("hidden")){
+    
+    if (nav.classList.contains("hidden")){ //if nav list is hidden, make it visible, switch buttons to close
         nav.classList.add("visible");
         nav.classList.remove("hidden");
+        menuButton.remove("visible");
+        menuButton.add("hidden");
+        closeMenuButton.add("visible");
+        closeMenuButton.remove("hidden");
     }else{
         nav.classList.add("hidden");
         nav.classList.remove("visible");
+        closeMenuButton.add("hidden");
+        closeMenuButton.remove("visible");
     }
 })
