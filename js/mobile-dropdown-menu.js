b876561c6b4b32729359d163c8ce434e4dfe48b7
Mewpet <claire_gaddhayward@hotmail.co.uk>
2026-06-30 12:29:11 +0100

    added close button to nav bar

--- js/mobile-dropdown-menu.js
+++ js/mobile-dropdown-menu.js
@@ -7,14 +7,10 @@ menuButton.addEventListener("click", ()=>{
     if (nav.classList.contains("hidden")){ //if nav list is hidden, make it visible, switch buttons to close
         nav.classList.add("visible");
         nav.classList.remove("hidden");
-        menuButton.remove("visible");
-        menuButton.add("hidden");
-        closeMenuButton.add("visible");
-        closeMenuButton.remove("hidden");
+        menuButton.innerHTML="Ⓧ";
     }else{
         nav.classList.add("hidden");
         nav.classList.remove("visible");
-        closeMenuButton.add("hidden");
-        closeMenuButton.remove("visible");
+        menuButton.innerHTML="≡";
     }
 })
