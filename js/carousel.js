d63f82a8d7e3c30ca2fdcf3bdb78886621215d7c
Mewpet <claire_gaddhayward@hotmail.co.uk>
2026-07-02 14:35:17 +0100

    have custom image showing in carousel

---
+++ js/carousel.js
@@ -0,0 +1,6 @@
+//pictures in the carousel
+let img1 = "../images/black and white forest.jpg";
+
+let carouselImage = document.getElementById("carousel-image");
+
+carouselImage.setAttribute("src",img1);
