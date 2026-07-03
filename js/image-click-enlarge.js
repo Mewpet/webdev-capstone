1aebb109722c0f8a44d042a2b77ab9e9e6649646
Mewpet <claire_gaddhayward@hotmail.co.uk>
2026-07-03 12:09:28 +0100

    Added fade in. alt text to enlarged image

--- js/image-click-enlarge.js
+++ js/image-click-enlarge.js
@@ -5,9 +5,15 @@ let enlargedImageDiv = document.getElementById("enlarged-image");
 //add click event to all the images
 images.forEach(image => {
     image.addEventListener("click",()=>{
-        enlargedImageDiv.classList.add("visible");
+        enlargedImageDiv.classList.add("visible-no-animation");
         enlargedImageDiv.classList.remove("hidden");
         enlargedImageDiv.appendChild(image.cloneNode());
-        
+
+        //add the alt text
+        let p = document.createElement("p");
+        p.innerText = image.getAttribute("alt");
+        p.setAttribute("id", "enlarged-image-description");
+        enlargedImageDiv.appendChild(p);
+        console.log(p);
     })
 });
